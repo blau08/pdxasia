077a2d2eca4443a422659ebc8135e7b423d875b0
Brian Lau <blau08@gmail.com>
2016-10-19 02:37:57 -0700

    add wells, limits

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,6 @@
 class WelcomeController < ApplicationController
   def index
-    @foods = Food.all.order(:name)
+    @foods = Food.all.order(:name).limit(8)
     @communities = Community.all.order(:name)
     @shops = Shop.all.order(:name)
   end

75b0c222ef9eb10ac41d5917bc3ef39bd4f0bfba
Brian Lau <blau08@gmail.com>
2016-10-24 12:17:39 -0700

    Add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+# file = File.open(File.join(Rails.root,'public/assets/foods/ec1.jpg'))
+food = Food.create(
+{
+  name: "EC Kitchen",
+  address: "6335 SE 82nd Ave, Portland, OR 97266",
+  phone: "(503) 788-6306",
+  yelp: "",
+  bio: "",
+  image: File.open(File.join(Rails.root,'public/assets/foods/ec1.jpg'))
+})
+
+food.pictures.create(image: File.open(File.join(Rails.root,'public/assets/foods/ec2.jpg')))
+food.pictures.create(image: File.open(File.join(Rails.root,'public/assets/foods/ec3.jpg')))
+food.pictures.create(image: File.open(File.join(Rails.root,'public/assets/foods/ec4.jpg')))

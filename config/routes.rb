710bf1237d7d6229d093cb007bb02264f455ec26
Brian Lau <blau08@gmail.com>
2016-11-10 01:29:29 -0800

    add video model

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,7 @@ Rails.application.routes.draw do
   resources :communities
   resources :events
   resources :pictures
+  resources :videos
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161110062930) do
+ActiveRecord::Schema.define(version: 20161110090149) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -94,6 +94,11 @@ ActiveRecord::Schema.define(version: 20161110062930) do
     t.string   "bio"
     t.string   "yelp"
     t.string   "phone"
+  end
+
+  create_table "videos", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end

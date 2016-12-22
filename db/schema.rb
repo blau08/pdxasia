b830aa76cfddb1c34ee20b4b1113c598946a0d12
Brian Lau <blau08@gmail.com>
2016-12-22 01:51:36 -0800

    add services website column

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161222094200) do
+ActiveRecord::Schema.define(version: 20161222095100) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -118,6 +118,7 @@ ActiveRecord::Schema.define(version: 20161222094200) do
     t.string   "language"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "website"
   end
 
   create_table "shops", force: :cascade do |t|

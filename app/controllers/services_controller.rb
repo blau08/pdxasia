4514f923878156696d4ed469b6dc2ce19bd91411
Brian Lau <blau08@gmail.com>
2016-12-22 02:05:40 -0800

    make website column work

--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -15,9 +15,26 @@ class ServicesController < ApplicationController
     @services = Service.all.order(:first)
   end
 
+  def show
+    @service = Service.find(params[:id])
+  end
+
+  def edit
+    @service = Service.find(params[:id])
+  end
+
+  def update
+    @service = Service.find(params[:id])
+    if @service.update(service_params)
+      redirect_to service_path(@service)
+    else
+      render :edit
+    end
+  end
+
   private
   def service_params
-    params.require(:service).permit(:first, :last, :number, :language, :industry)
+    params.require(:service).permit(:first, :last, :number, :language, :industry, :website)
   end
 
 end

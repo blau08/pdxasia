df70ae0398b7330afb804fc9a1042d6ba5b2134c
Brian Lau <blau08@gmail.com>
2017-02-23 01:59:23 -0800

    add google analytics tracker code

--- Gemfile
+++ Gemfile
@@ -14,6 +14,7 @@ gem 'font-awesome-sass'
 gem 'rails-i18n', '~> 4.0.0' # For 4.0.x
 gem 'globalize', '~> 5.0.0'
 gem 'social-share-button'
+gem 'google-analytics-rails', '1.1.0'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.2.5'
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,8 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  # replace this with your tracker code
+  GA.tracker = "UA-67372952-3"
   # Code is not reloaded between requests.
   config.cache_classes = true
 

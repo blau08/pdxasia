21dc738c570f4ccb89ead49b3bd8af27277ee940
Brian Lau <blau08@gmail.com>
2017-04-03 17:39:47 -0700

    test

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,11 +3,13 @@ class WelcomeController < ApplicationController
 
   def index
     # @foods = Food.all.limit(12)
-    @foods = Food.joins('INNER JOIN food_translations ON foods.id=food_translations.food_id').order('food_translations.name').where('food_translations.locale=?', params[:locale])
+    @foods = Food.joins('INNER JOIN food_translations ON foods.id=food_translations.food_id').order('food_translations.name').where('food_translations.locale=?', params[:locale]).limit(3)
     # @foods = Food.all.order(:name).limit(12)
     @communities = Community.all.order(:name)
-    @shops = Shop.all.order(:name)
-    @events = Event.all.limit(6)
+    @shops = Shop.all.order(:name).limit(3)
+    @eventsbox = Event.all.limit(3)
+    @events = Event.all
+    @services = Service.all.limit(3)
   end
 
   # def set_locale
